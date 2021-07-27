830467f6d1bbe13ea6c586dffc91098226c4237f
VSHS <VSHS@INPU51YMBH2.inbe.capco.com>
2021-07-27 18:49:54 +0530

    new changes in login page

--- Framework_New/src/test/java/com/learnautomation/pages/LoginPage.java
+++ Framework_New/src/test/java/com/learnautomation/pages/LoginPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+// this is new cahnge
 public class LoginPage {
 	WebDriver driver;
 	public LoginPage(WebDriver ldriver) {
